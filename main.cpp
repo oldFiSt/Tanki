a29f5182393f508a22e8f146128d67bff05d61f5
Sergei <skozlenko409@gmail.com>
2024-06-30 15:36:07 +0700

    new

--- main.cpp
+++ main.cpp
@@ -1,16 +1,47 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include<iostream>
+#include <iostream>
 
-int g_WindowSizeX = 640;
-int g_WindowSizeY = 480;
+GLfloat point[] = {
+     0.0f,  0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f
+};
 
-void glfwWindowSizeCallback(GLFWwindow* pWindow, int widht, int height)
+GLfloat colors[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+
+const char* vertex_shader =
+"#version 460\n"
+"layout(location = 0) in vec3 vertex_position;"
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color;"
+"void main() {"
+"   color = vertex_color;"
+"   gl_Position = vec4(vertex_position, 1.0);"
+"}";
+
+const char* fragment_shader =
+"#version 460\n"
+"in vec3 color;"
+"out vec4 frag_color;"
+"void main() {"
+"   frag_color = vec4(color, 1.0);"
+"}";
+
+
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height)
 {
-    g_WindowSizeX = widht;
-    g_WindowSizeY = height;
-    glViewport(0, 0, g_WindowSizeX/2, g_WindowSizeY);//Где мы хотим рисовать(в какой области окна). Изначально левый нижний угол окна
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
 }
 
 void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode)
@@ -26,7 +57,7 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
     {
-        std::cout<<"glfwInit failed!"<<std::endl;
+        std::cout << "glfwInit failed!" << std::endl;
         return -1;
     }
 
@@ -35,10 +66,10 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    GLFWwindow* pWindow = glfwCreateWindow(g_WindowSizeX, g_WindowSizeY, "Battle City", nullptr, nullptr);
+    GLFWwindow* pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Battle City", nullptr, nullptr);
     if (!pWindow)
     {
-        std::cout<<"glfwCreateWindow falied!"<<std::endl;    
+        std::cout << "glfwCreateWindow failed!" << std::endl;
         glfwTerminate();
         return -1;
     }
@@ -51,22 +82,64 @@ int main(void)
 
     if (!gladLoadGL())
     {
-        std::cout<<"Can't load GLAD!"<<std::endl;
-        return -1;
+        std::cout << "Can't load GLAD!" << std::endl;
     }
-    std::cout<<"Renderer: "<<glGetString(GL_RENDERER)<<std::endl;
-    std::cout<<"OpenGL version: "<<glGetString(GL_VERSION)<<std::endl;
+
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
     glClearColor(1, 1, 0, 1);
 
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, nullptr);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, nullptr);
+    glCompileShader(fs);
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vs);
+    glAttachShader(shader_program, fs);
+    glLinkProgram(shader_program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    GLuint points_vbo = 0;
+    glGenBuffers(1, &points_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(pWindow))//Цикл отрисовки(рендеринг(отрисовка на экране))
+    while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);//Очищаем буфер цвета
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(pWindow);//то есть меняет местами передний и задний буфер цвета
+        glfwSwapBuffers(pWindow);
 
         /* Poll for and process events */
         glfwPollEvents();
